efeab0ee5e7471a62250469675b445dc50e88677
WLuLi <2970012705@qq.com>
2023-07-30 12:55:43 +0800

    backend update

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // app.js
-App({})
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('云服务错误')
+    } else {
+      wx.cloud.init({
+        env: 'qucikpartyproject-2e7ne84e8accb7',
+        traceUser: true,
+      })
+    }
+    this.globalData = {}
+  }
+})
--- backend/app/extensions/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger
-
-db = SQLAlchemy()
-swagger = Swagger()
